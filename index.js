10fcfcb1ed39033f549155b9f7818b68620c4052
Aryan Namdev <aryanamdev08@gmail.com>
2022-09-16 02:58:20 +0530

    Edited Html

---
+++ index.js
@@ -0,0 +1,25 @@
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(
+      `audio[data-key='${e.keyCode}']`
+    );
+    const key = document.querySelector(
+      `.key[data-key='${e.keyCode}']`
+    );
+    audio.play();
+    audio.currentTime = 0; //rewind to the start
+    console.log(audio);
+    key.classList.add('playing');
+
+  });
+
+  
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing')
+  }
+
+  const keys = document.querySelectorAll('.key')
+
+  keys.forEach(key => 
+    key.addEventListener('transitionend', removeTransition)
+  );
